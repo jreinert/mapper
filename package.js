ca8f79f249217626b863744ff229f62b90a1c8e5
Joakim Reinert <mail@jreinert.com>
2015-06-29 12:00:10 +0200

    add initial package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+	name: 'mapper',
+	version: '0.0.1',
+	summary: 'An ODM (Object-Document Mapper) for Meteor',
+	git: 'https://github.com/jreinert/mapper.git',
+	documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.1.0.2');
+});
