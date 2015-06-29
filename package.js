d44ca914ddd4d5102d90b271f22c8d6078ca9d6c
Joakim Reinert <mail@jreinert.com>
2015-06-29 12:40:00 +0200

    prefix package name with github username

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-	name: 'mapper',
+	name: 'jreinert:mapper',
 	version: '0.0.1',
 	summary: 'An ODM (Object-Document Mapper) for Meteor',
 	git: 'https://github.com/jreinert/mapper.git',
